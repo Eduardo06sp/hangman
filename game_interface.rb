5204f9810e0da4dfc3e6ae2758b7a71b3756da3a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-17 13:48:55 -0700

    Display separator to distinguish each round in terminal interface

--- game_interface.rb
+++ game_interface.rb
@@ -1,6 +1,13 @@
 #frozen_string_literal: true
 
 module GameInterface
+  def display_separator
+    puts <<-HEREDOC
+    -------------------------------------
+    -------------------------------------
+    HEREDOC
+  end
+
   def display_hint
     puts <<-HEREDOC
     Secret Word: #{secret_hint.join('   ')}
