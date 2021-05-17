4a656686186df55415b1fd824527371a2496ba32
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-05-17 13:39:24 -0700

    Draw hangman depending on guesses left

--- game_interface.rb
+++ game_interface.rb
@@ -6,4 +6,63 @@ module GameInterface
     Secret Word: #{secret_hint.join('   ')}
     HEREDOC
   end
+
+  def display_hangman
+    case guesses_left
+    when 6
+      man =
+        "│
+    │
+    │
+    │               "
+    when 5
+      man =
+        "│            O
+    │
+    │
+    │               "
+    when 4
+      man =
+        "│            O
+    │            |
+    │            |
+    │               "
+    when 3
+      man =
+        "│            O
+    │           /|
+    │            |
+    │               "
+    when 2
+      man =
+        "│            O
+    │           /|\\
+    │            |
+    │               "
+    when 1
+      man =
+        "│            O
+    │           /|\\
+    │            |
+    │           /   "
+    when 0
+      man =
+        "│            O
+    │           /|\\
+    │            |
+    │           / \\"
+    end
+
+    puts <<-HEREDOC
+    │────────────│
+    │            │
+    │            │
+    #{man}
+    │
+    │──────│
+    │      │───────
+    │              │
+     ──────────────
+    HEREDOC
+  end
 end
